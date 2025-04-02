ffe992da07d8aadb96c777173518dce47f477048
JrSamuraison <JrSamuraison@users.noreply.github.com>
2025-04-02 17:54:18 +0000

    Fix sum_of_multiples to correctly handle limit and overlapping multiples

--- src/sum_of_multiples.py
+++ src/sum_of_multiples.py
@@ -28,8 +28,11 @@ def sum_of_multiples(limit, multiples):
     
     # Find multiples for each number in the multiples list
     for multiple in multiples:
-        # Add all multiples of the current number up to the limit
-        unique_multiples.update(range(multiple, limit + 1, multiple))
+        # Add multiples of the current number up to (but not exceeding) the limit
+        current_multiple = multiple
+        while current_multiple <= limit:
+            unique_multiples.add(current_multiple)
+            current_multiple += multiple
     
     # Return the sum of unique multiples
     return sum(unique_multiples)
